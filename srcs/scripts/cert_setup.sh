8f9696810c49bb250e06535314fb0198abf0de82
seonlim <dltjwl1@naver.com>
2024-05-03 16:52:15 +0900

    added filebeat

--- srcs/scripts/cert_setup.sh
+++ srcs/scripts/cert_setup.sh
@@ -28,6 +28,24 @@ if [ ! -f config/certs/certs.zip ]; then
         "       - localhost\n"\
         "     ip:\n"\
         "       - 127.0.0.1\n"\
+        "   - name: logstash\n"\
+        "     dns:\n"\
+        "       - logstash\n"\
+        "       - localhost\n"\
+        "     ip:\n"\
+        "       - 127.0.0.1\n"\
+        "   - name: kibana\n"\
+        "     dns:\n"\
+        "       - kibana\n"\
+        "       - localhost\n"\
+        "     ip:\n"\
+        "       - 127.0.0.1\n"\
+        "   - name: filebeat\n"\
+        "     dns:\n"\
+        "       - filebeat\n"\
+        "       - localhost\n"\
+        "     ip:\n"\
+        "       - 127.0.0.1\n"\
     > config/certs/instances.yml;
     bin/elasticsearch-certutil cert --silent --pem -out config/certs/certs.zip --in config/certs/instances.yml --ca-cert config/certs/ca/ca.crt --ca-key config/certs/ca/ca.key;
     unzip config/certs/certs.zip -d config/certs;
@@ -49,8 +67,7 @@ until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASS
     do sleep 5;
 done;
 echo "Setting logstash_internal account";
-until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://es01:9200/_security/user/logstash_internal -d '{"password" : "x-pack-test-password", "roles" : [ "logstash_writer"], "full_name" : "Internal Logstash User"}' | grep -q '{"created":true}';
+until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://es01:9200/_security/user/logstash_internal -d "{\"password\" : \"${LOGSTASH_INTERNAL_PASSWORD}\", \"roles\" : [ \"logstash_writer\"], \"full_name\" : \"Internal Logstash User\"}" | grep -q '{"created":true}';
     do sleep 5;
 done;
 echo "\033[34mDone!\033[0m";
-# curl -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://es01:9200/_security/role/logstash_writer -d '{"cluster": ["manage_index_templates", "monitor", "manage_ilm"], "indices": [{"names": [ "logstash-*" ], "privileges": ["write","create","create_index","manage","manage_ilm"]}]}'
