d47599d347679c1d5a0a9730a6b52c793cbb0bc2
seonlim <dltjwl1@naver.com>
2024-05-14 19:53:55 +0900

    set logstash_writer role

--- srcs/scripts/cert_setup.sh
+++ srcs/scripts/cert_setup.sh
@@ -69,7 +69,7 @@ until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASS
     do sleep 5;
 done;
 echo "Setting logstash_writer role";
-until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://es01:9200/_security/role/logstash_writer -d '{"cluster": ["manage_index_templates", "monitor", "manage_ilm"], "indices": [{"names": [ "logstash-*" ], "privileges": ["write","create","create_index","manage","manage_ilm"]}]}' | grep -q '{"role":{"created":true}}';
+until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://es01:9200/_security/role/logstash_writer -d '{"cluster": ["manage_index_templates", "monitor", "manage_ilm"], "indices": [{"names": [ "log-*" ], "privileges": ["write","create","create_index","manage","manage_ilm"]}]}' | grep -q '{"role":{"created":true}}';
     do sleep 5;
 done;
 echo "Setting logstash_internal account";
