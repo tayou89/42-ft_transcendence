2a3b60728c86bd7705e87d9aa6119c3f01399144
sayoon <sayoon@student.42seoul.kr>
2024-05-16 18:07:12 +0900

    Add: 소켓통신 consumer 추가 - asgi 에서 사용하는 consumer 추가 - game_room 저장할 model 추가

--- srcs/requirements/game_server/utils/setup_server.sh
+++ srcs/requirements/game_server/utils/setup_server.sh
@@ -2,9 +2,11 @@
 
 cd django
 
-# python3 manage.py makemigrations
-# python3 manage.py makemigrations api
+export DJANGO_SETTINGS_MODULE=gameserver.settings
 
-# python3 manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py makemigrations api
+
+python3 manage.py migrate
 
 uvicorn gameserver.asgi:application --host 0.0.0.0 --port 8000
