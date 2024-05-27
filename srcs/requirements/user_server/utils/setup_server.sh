0b99d57cb65a0ce23fdae742ad92586ffc2083c2
seonlim <dltjwl1@naver.com>
2024-05-27 21:21:31 +0900

    fixed gunicorn external url bug

--- srcs/requirements/user_server/utils/setup_server.sh
+++ srcs/requirements/user_server/utils/setup_server.sh
@@ -8,4 +8,4 @@ python3 manage.py makemigrations api
 python3 manage.py migrate
 
 # python3 manage.py runserver 0.0.0.0:8000
-gunicorn --bind 0.0.0.0:8000 user_manage.wsgi:application
+gunicorn -k gevent --workers 4 --bind 0.0.0.0:8000 user_manage.wsgi:application
