a9a868a79da07a7e89025fd02c3aac58723ec844
seonlim <dltjwl1@naver.com>
2024-06-05 15:30:08 +0900

    divided certs_setup

--- srcs/requirements/elasticsearch/cert_setup.sh
+++ srcs/requirements/elasticsearch/cert_setup.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-if [ x${ELASTIC_PASSWORD} == x ]; then
-    echo "Set the ELASTIC_PASSWORD environment variable in the .env file";
-    exit 1;
-elif [ x${KIBANA_PASSWORD} == x ]; then
-    echo "Set the KIBANA_PASSWROD environment variable in the .env file";
-    exit 1;
-fi
 if [ ! -f config/certs/ca.zip ]; then
     echo "Creating CA";
     bin/elasticsearch-certutil ca --silent --pem -out config/certs/ca.zip;
@@ -68,24 +61,9 @@ if [ ! -f config/certs/certs.zip ]; then
     bin/elasticsearch-certutil cert --silent --pem -out config/certs/certs.zip --in config/certs/instances.yml --ca-cert config/certs/ca/ca.crt --ca-key config/certs/ca/ca.key;
     unzip config/certs/certs.zip -d config/certs;
 fi
-# echo "Setting file permissions"
-# chown -R root:root config/certs;
-# find . -type d -exec chmod 755 \{\} \;;
-# find . -type f -exec chmod 644 \{\} \;;
-# echo "Waiting for Elasticsearch availability";
-# until curl -s --cacert config/certs/ca/ca.crt https://es01:9200 | grep -q "missing authentication credentials";
-#     do sleep 5;
-# done;
-# echo "Setting kibana_system password";
-# until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://es01:9200/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_PASSWORD}\"}" | grep -q "^{}";
-#     do sleep 5;
-# done;
-# echo "Setting logstash_writer role";
-# until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://es01:9200/_security/role/logstash_writer -d '{"cluster": ["manage_index_templates", "monitor", "manage_ilm"], "indices": [{"names": [ "log-*" ], "privileges": ["write","create","create_index","manage","manage_ilm"]}]}' | grep -q '{"role":{"created":true}}';
-#     do sleep 5;
-# done;
-# echo "Setting logstash_internal account";
-# until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://es01:9200/_security/user/logstash_internal -d "{\"password\" : \"${LOGSTASH_INTERNAL_PASSWORD}\", \"roles\" : [ \"logstash_writer\"], \"full_name\" : \"Internal Logstash User\"}" | grep -q '{"created":true}';
-#     do sleep 5;
-# done;
-# echo "\033[34mDone!\033[0m";
+echo "Setting file permissions"
+chown -R root:root config/certs;
+find . -type d -exec chmod 755 \{\} \;;
+find . -type f -exec chmod 644 \{\} \;;
+
+echo "====================DONE===================="
