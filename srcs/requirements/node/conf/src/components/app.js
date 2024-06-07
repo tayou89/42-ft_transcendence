cc6728d1f72e9d86ac501b68d319049be2416f8e
Byeongmin Jeon <byejeon@c5r6s3.42seoul.kr>
2024-06-07 17:42:52 +0900

    testing

--- srcs/requirements/node/conf/src/components/app.js
+++ srcs/requirements/node/conf/src/components/app.js
@@ -1,17 +1,23 @@
 import MyReact from "../MyReact/MyReact.js";
+import Test from "./test.js";
 import Home from "./home.js";
 import { Route } from "../MyReact/MyReactRouter.js";
 import Login from "./login.js";
 import Room from "./room.js";
 
 function App() {
-  return (
-    <div>
-      <Route path="/" component={Home} />
+	return (
+		<div>
+			{/* <Route path="/" component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/room" component={Room} />
-    </div>
-  );
+	<div className="container p-5 my-5 border">*/}
+
+			<Test.Navbar_test name="byejeon"/>
+			{/* <Test.Home_test/> */}
+			{/* <Test.Login_test/> */}
+		</div>
+	);
 }
 
 export default App;
