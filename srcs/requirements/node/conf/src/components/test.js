9c30e7110706f7d830c8862acf14b1ab593cddaa
Byeongmin Jeon <byejeon@c5r6s3.42seoul.kr>
2024-06-07 17:42:52 +0900

    UserPage 0.1v

--- srcs/requirements/node/conf/src/components/test.js
+++ srcs/requirements/node/conf/src/components/test.js
@@ -1,8 +1,5 @@
 import MyReact from "../MyReact/MyReact.js";
 
-function clickLoginBtn() {
-	window.location.href = "http://localhost:8000/api/login/";
-}
 
 // size : lg sm ''
 function Btn({ size, text, onClickFunc }) {
@@ -23,6 +20,10 @@ function Btn({ size, text, onClickFunc }) {
 	);
 }
 
+function clickLoginBtn() {
+	window.location.href = "http://localhost:8000/api/login/";
+}
+
 function Login_test() {
 	return (
 		<div className="mt-5">
@@ -38,7 +39,7 @@ function Login_test() {
 	);
 }
 
-function Navbar_test({ name, phoneNumber }) {
+function Navbar_test({ name }) {
 	return (
 		<nav className="navbar navbar-expand-sm bg-dark">
 			<div className="col-sm-5"></div>
@@ -49,18 +50,96 @@ function Navbar_test({ name, phoneNumber }) {
 			</div>
 			<div className="col-sm-3"></div>
 			<div className="container col-sm-2 justify-content-center">
-					<img className="rounded-circle"
+				<img className="rounded-circle"
 					width="30" height="30"
 					src="https://www.studiopeople.kr/common/img/default_profile.png"></img>
-					<a className="nav-link text-white ps-2" href="#">byejeon</a>
+				<a className="nav-link text-white ps-2" href="#">{name}</a>
 			</div>
 		</nav>
+	);
+}
+
+function Home_test({ name }) {
+	return (
+		<div>
+			<Test.Navbar_test name="byejeon" />
+			<div className="row p-3 m-3 border">
+				<div className="col-md-5">
+					<div className="container mt-3 mb-3 pt-2 pb-2 border">
+						<div className="container mt-1 text-light border">User</div>
+						<div className="container mt-1 text-light border">User</div>
+						<div className="container mt-1 text-light border">User</div>
+						<div className="container mt-1 text-light border">User</div>
+						<div className="container mt-1 text-light border">User</div>
+						<div className="container mt-1 text-light border">User</div>
+					</div>
+					<div className="container mt-3 mb-3 pt-2 pb-2 border">
+						<div className="container mt-1 text-light border">Friend</div>
+						<div className="container mt-1 text-light border">Friend</div>
+						<div className="container mt-1 text-light border">Friend</div>
+						<div className="container mt-1 text-light border">Friend</div>
+						<div className="container mt-1 text-light border">Friend</div>
+						<div className="container mt-1 text-light border">Friend</div>
+						<div className="container mt-1 text-light border">Friend</div>
+					</div>
+				</div>
+				<div className="col-md-7">
+					<div className="container mt-3 pt-2 pb-2 border">
+						<div className="container text-light mt-1 pt-1 pb-1 border">Match</div>
+						<div className="container text-light mt-1 pt-1 pb-1 border">Match</div>
+						<div className="container text-light mt-1 pt-1 pb-1 border">Match</div>
+						<div className="container text-light mt-1 pt-1 pb-1 border">Match</div>
+						<div className="container text-light mt-1 pt-1 pb-1 border">Match</div>
+						<div className="container text-light mt-1 pt-1 pb-1 border">Match</div>
+						<div className="container text-light mt-1 pt-1 pb-1 border">Match</div>
+						<div className="container text-light mt-1 pt-1 pb-1 border">Match</div>
+						<div className="container text-light mt-1 pt-1 pb-1 border">Match</div>
+						<div className="container text-light mt-1 pt-1 pb-1 border">Match</div>
+						<div className="container text-light mt-1 pt-1 pb-1 border">Match</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+function UserPage_test() {
+	return (
+		<div>
+			<Test.Navbar_test name="byejeon" />
+			<div className="row p-3 m-3 border">
+				<div className="col-md-12">
+					<div className="container mt-3 pt-2 pb-2 ms-0 me-0 text-light border">
+						<div>User Info</div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-md-6">
+						<div className="container mt-3 pt-2 pb-2 text-light border">
+							<div className="border text-light mt-1 pt-1 pb-1 ">Win</div>
+						</div>
+					</div>
+					<div className="col-md-6">
+						<div className="container mt-3 pt-2 pb-2 border">
+							<div className="border text-light mt-1 pt-1 pb-1 ">Match Record</div>
+							<div className="border text-light mt-1 pt-1 pb-1 ">Match Record</div>
+							<div className="border text-light mt-1 pt-1 pb-1 ">Match Record</div>
+							<div className="border text-light mt-1 pt-1 pb-1 ">Match Record</div>
+							<div className="border text-light mt-1 pt-1 pb-1 ">Match Record</div>
+							<div className="border text-light mt-1 pt-1 pb-1 ">Match Record</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 }
 
 const Test = {
 	Login_test,
 	Navbar_test,
+	Home_test,
+	UserPage_test
 };
 
 export default Test;
