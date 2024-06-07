5217bbf53156cadf6f5f6d3a14779accd4e39b1d
Dodontak <mu03019@naver.com>
2024-06-07 17:42:52 +0900

    Navbar를 Bootstrap Navbar로 변경
    기존엔 container-fluid, row, col로 navbar의 내용을 분리했는데
    Bootstrap에서 제공하는 navBar class를 이용하는것으로 변경함.

--- srcs/requirements/node/conf/src/components/Navbar.js
+++ srcs/requirements/node/conf/src/components/Navbar.js
@@ -5,22 +5,20 @@ import { navigate } from "../MyReact/MyReactRouter.js";
 
 function Navbar({ name }) {
 	return (
-		<div className="container-fluid border-bottom text-center my-1">
-			<div className="container my-2">
-				<div className="row">
-					<div className="container col fs-3">
-						<Link to="/" className="navbar-brand ps-2 text-light">42 Pong</Link>
+		<div className="container-fluide border-bottom">
+			<div className="container">
+				<nav className="navbar navbar-expand-sm navbar-dark">
+					<div className="container-fluid">
+						<Link to="/" className="navbar-brand">42 Pong</Link>
+						<Link to="/login" className="navbar-brand">로그인</Link>
+						<div>
+							<img className="rounded-circle"
+								width="30" height="30"
+								src="https://www.studiopeople.kr/common/img/default_profile.png" />
+							<Link to="/profile" className="navbar-brand ps-2 text-light">{name}</Link>
+						</div>
 					</div>
-					<div className="container col fs-5">
-						<Link to="/login" className="navbar-brand ps-2 text-light">login</Link>
-					</div>
-					<div className="container col-3 my-2 text-end">
-						<img className="rounded-circle"
-							width="30" height="30"
-							src="https://www.studiopeople.kr/common/img/default_profile.png" />
-						<Link to="/profile" className="navbar-brand ps-2 text-light">{name}</Link>
-					</div>
-				</div>
+				</nav>
 			</div>
 		</div>
 	);
