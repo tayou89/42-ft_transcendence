3074088b45cc39135fa744a6b72595eb3dcc47d5
Byeongmin Jeon <byejeon@c5r6s3.42seoul.kr>
2024-06-07 17:42:52 +0900

    webSocket test start

--- srcs/requirements/node/conf/src/components/app.js
+++ srcs/requirements/node/conf/src/components/app.js
@@ -8,13 +8,12 @@ import Room from "./room.js";
 function App() {
 	return (
 		<div>
-			{/* <Route path="/" component={Home} />
+			<Route path="/" component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/room" component={Room} />
-	<div className="container p-5 my-5 border">*/}
 
-			<Test.Home_test name="byejeon"/>
-			<Test.UserPage_test name="byejeon"/>
+			{/* <Test.Home_test name="byejeon"/> */}
+			{/* <Test.UserPage_test name="byejeon"/> */}
 			{/* <Test.Login_test/> */}
 		</div>
 	);
