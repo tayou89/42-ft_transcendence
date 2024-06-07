3885755523a0e1bbe3db8aa89cf863636ca34c7a
Byeongmin Jeon <byejeon@c5r6s3.42seoul.kr>
2024-06-07 17:42:52 +0900

    front updated

--- srcs/requirements/node/conf/src/components/app.js
+++ srcs/requirements/node/conf/src/components/app.js
@@ -8,14 +8,16 @@ import Room from "./room.js";
 function App() {
 	return (
 		<div>
+			<Route path="/" component={Test.Home_test} />
+			<Route path="/profile" component={Test.UserPage_test} />
+			{/* <Route path="/" component={Test.Home_test} /> */}
 			{/* <Test.Home_test name="byejeon"/> */}
 			{/* <Test.UserPage_test name="byejeon"/> */}
 			{/* <Test.Login_test/> */}
-			<Test.CeateMatch_test/>
+			{/* <Test.CeateMatch_test/> */}
 			{/* <Route path="/" component={Home} /> */}
       {/* <Route path="/login" component={Login} /> */}
       {/* <Route path="/room" component={Room} /> */}
-
 		</div>
 	);
 }
