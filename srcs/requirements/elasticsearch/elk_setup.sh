4089c3a01c50d3629401fb67837e5ea94d454bc0
seonlim <dltjwl1@naver.com>
2024-06-10 22:50:19 +0900

    WIP: kibana export

--- srcs/requirements/elasticsearch/elk_setup.sh
+++ srcs/requirements/elasticsearch/elk_setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ES_URL="https://es01:9200";
+KIBANA_URL="https://kibana:5601";
+
 if [ x${ELASTIC_PASSWORD} == x ]; then
     echo "Set the ELASTIC_PASSWORD environment variable in the .env file";
     exit 1;
@@ -16,15 +19,24 @@ until curl -s --cacert config/certs/ca/ca.crt https://es01:9200 | grep -q "missi
     do sleep 5;
 done;
 echo "Setting kibana_system password";
-until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://es01:9200/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_PASSWORD}\"}" | grep -q "^{}";
+until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" ${ES_URL}/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_PASSWORD}\"}" | grep -q "^{}";
     do sleep 5;
 done;
 echo "Setting logstash_writer role";
-until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://es01:9200/_security/role/logstash_writer -d '{"cluster": ["manage_index_templates", "monitor", "manage_ilm"], "indices": [{"names": [ "log-*" ], "privileges": ["write","create","create_index","manage","manage_ilm"]}]}' | grep -q '{"role":{"created":true}}';
+until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" ${ES_URL}/_security/role/logstash_writer -d '{"cluster": ["manage_index_templates", "monitor", "manage_ilm"], "indices": [{"names": [ "log-*" ], "privileges": ["write","create","create_index","manage","manage_ilm"]}]}' | grep -q '{"role":{"created":true}}';
     do sleep 5;
 done;
 echo "Setting logstash_internal account";
-until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://es01:9200/_security/user/logstash_internal -d "{\"password\" : \"${LOGSTASH_INTERNAL_PASSWORD}\", \"roles\" : [ \"logstash_writer\"], \"full_name\" : \"Internal Logstash User\"}" | grep -q '{"created":true}';
+until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" ${ES_URL}/_security/user/logstash_internal -d "{\"password\" : \"${LOGSTASH_INTERNAL_PASSWORD}\", \"roles\" : [ \"logstash_writer\"], \"full_name\" : \"Internal Logstash User\"}" | grep -q '{"created":true}';
     do sleep 5;
 done;
+echo "Setting log policies";
+until curl -s -X PUT --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" ${ES_URL}/_ilm/policy/django-policy -d '{"policy":{"phases":{"hot":{"min_age":"0ms","actions":{"rollover":{"max_age":"30d","max_size":"1gb","max_primary_shard_size":"1gb"}}},"warm":{"min_age":"1m","actions":{"set_priority":{"priority":50}}},"cold":{"min_age":"5m","actions":{"set_priority":{"priority":0}}},"delete":{"min_age":"20m","actions":{"delete":{"delete_searchable_snapshot":true}}}}}}' | grep -q '{"acknowledged":true}';
+    do sleep 5;
+done;
+echo "Importing kibana settings";
+until curl -s -X POST --cacert config/certs/ca/ca.crt ${KIBANA_URL}/api/saved_objects/_import -H "kbn-xsrf: true" --form file=@/settings/kibana_objects.ndjson
+    do sleep 5;
+done;
+
 echo "\033[34mDone!\033[0m";
