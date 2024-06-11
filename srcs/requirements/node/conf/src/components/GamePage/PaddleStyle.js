81c22bdd2a9e5bd3882bb93ce7505b8f860ec685
tayou89 <ysh00718@gmail.com>
2024-06-11 23:06:39 +0900

    Add: game paddle 추가

---
+++ srcs/requirements/node/conf/src/components/GamePage/PaddleStyle.js
@@ -0,0 +1,52 @@
+import {BOARD, PADDLE} from "./constant.js";
+
+class PaddleStyle {
+    constructor(id) {
+        this.#setPaddleNumber(id);
+        this.#setX(id);
+        this.#y = PADDLE.INITIAL_Y;
+        this.#width = PADDLE.WIDTH;
+        this.#height = PADDLE.HEIGHT;
+        this.#radius = PADDLE.BORDER_RADIUS;
+        this.#color = PADDLE.COLOR;
+    }
+    #setX(id) {
+        if (id === "paddle1")
+            this.#x = PADDLE.OFFSET_X - (PADDLE.WIDTH / 2);
+        else    
+            this.#x = BOARD.WIDTH - (PADDLE.OFFSET_X + (PADDLE.WIDTH / 2));
+    }
+    #setY(value = this.#y) {
+        this.#y = value;
+    }
+    #setPaddleNumber(id){
+        if (id === "paddle1")
+            this.#paddleNumber = 1;
+        else    
+            this.#paddleNumber = 2;
+    }
+    getStyle(valueY = undefined) {
+        this.#setY(valueY);
+        const gradientDirection = (this.#paddleNumber === 1 ? "left" : "right");
+        const style = {
+            position: `absolute`,
+            backgroundColor: `${this.#color}px`,
+            width: `${this.#width}px`,
+            height: `${this.#height}px`,
+            left: `${this.#x}px`,
+            top: `${this.#y}px`,
+            borderRadius: `${this.#radius}px`,
+            background: `linear-gradient(to ${gradientDirection}, ${this.#color} 50%, black 100%)`,
+        };
+        return (style);
+    }
+    #x;
+    #y;
+    #width;
+    #height;
+    #radius;
+    #color;
+    #paddleNumber;
+}
+
+export default PaddleStyle;
--- srcs/requirements/node/conf/src/components/GamePage/constant.js
+++ srcs/requirements/node/conf/src/components/GamePage/constant.js
@@ -8,6 +8,8 @@ export const PADDLE = {
     HEIGHT: 40,
     OFFSET_X: 40,
     INITIAL_Y:  (BOARD.HEIGHT / 2) - (40 / 2),
+    BORDER_RADIUS: 3,
+    COLOR: "white"
 };
 
 export const BALL = {
