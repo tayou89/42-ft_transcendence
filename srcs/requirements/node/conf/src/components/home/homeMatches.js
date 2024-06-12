5a03bf2172962ec78154b3b94fe88adc387ecf14
Dodontak <mu03019@naver.com>
2024-06-12 16:35:26 +0900

    modify typo matchs to matches

--- srcs/requirements/node/conf/src/components/home/homeMatches.js
+++ srcs/requirements/node/conf/src/components/home/homeMatches.js
@@ -1,7 +1,7 @@
 import { useEffect, useState, MyReact } from "../../MyReact/MyReact.js";
 import { navigate } from "../../MyReact/MyReactRouter.js";
 
-function HomeMatchs({ matchData }) {
+function HomeMatches({ matchData }) {
 	return (
 		<div className="container pt-2 pb-2 border-top border-bottom">
 			<div className="container my-3">Match</div>
@@ -17,4 +17,4 @@ function HomeMatchs({ matchData }) {
 	);
 }
 
-export default HomeMatchs;
+export default HomeMatches;
