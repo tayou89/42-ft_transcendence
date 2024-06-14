e90d55935d9b1f52fe0ce84045910cbf856593a4
tayou89 <ysh00718@gmail.com>
2024-06-14 15:25:23 +0900

    Fix: Fetch.myData -> Fetch.userData 로 기능 및 이름 변경

--- srcs/requirements/node/conf/src/components/Fetch/Fetch.js
+++ srcs/requirements/node/conf/src/components/Fetch/Fetch.js
@@ -1,15 +1,15 @@
 import { DEFAULT_URL} from "./constant.js";
 
 class Fetch {
-    static async myData() {
+    static async userData(id = "me") {
         try {
-            const url = `${DEFAULT_URL}/api/users/me`;
+            const url = `${DEFAULT_URL}/api/users/${id}`;
             const details = { method: 'GET', credentials: 'include' };
             const response = await fetch(url, details);
             const data = await response.json();
 
-            if (!response.ok)
-                throw new Error(`response isn't ok for url ${url}`);
+            if (data.detail)
+				tokenRefreshAndGoTo("/home");
             data.photoURL = await this.#photoURL(`/api/users/${data.id}/avatar`);
             return (data);
         }
