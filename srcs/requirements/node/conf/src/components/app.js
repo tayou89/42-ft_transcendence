05b067760b738154afa591fca1f23fa5db9cbc82
Dodontak <mu03019@naver.com>
2024-06-14 19:24:32 +0900

    add emailOTP

--- srcs/requirements/node/conf/src/components/app.js
+++ srcs/requirements/node/conf/src/components/app.js
@@ -4,11 +4,13 @@ import Home from "./home/home.js";
 import UserPage from "./UserPage.js";
 import Login from "./login.js";
 import Interchange from "./interchange.js";
+import EmailOTP from "./emailOTP.js";
 
 function App() {
 	return (
 		<div>
 			<Route path="/" component={Interchange} />
+			<Route path="/emailotp" component={EmailOTP} />
 			<Route path="/home" component={Home} />
 			<Route path="/profile" component={UserPage} />
 			<Route path="/login" component={Login} />
