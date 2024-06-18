3a6698e06eec676413f481cfecf51fa15cdeace2
Dodontak <mu03019@naver.com>
2024-06-18 19:00:14 +0900

    change Navbar link to dropdown

--- srcs/requirements/node/conf/src/components/Navbar.js
+++ srcs/requirements/node/conf/src/components/Navbar.js
@@ -1,19 +1,44 @@
 import MyReact from "../MyReact/MyReact.js";
 import MyReactRouter, { Link } from "../MyReact/MyReactRouter.js";
 import getCookieValue from "./utility/getCookieValue.js";
+import { navigate } from "../MyReact/MyReactRouter.js";
+
+function onClickShowMyInfo() {
+	navigate("/profile");
+}
+
+function onClickLogout() {
+	const logoutApiUrl = "http://localhost:8000/api/logout";
+	fetch(logoutApiUrl, {
+		method: 'POST',
+		credentials: 'include'
+	})
+		.then(console.log)
+		.catch(console.log);
+}
 
 function Navbar({ name, profileImg }) {
 	return (
-		<div className="container-fluide border-bottom">
+		<div className="container-fluide border-bottom" style="user-select: none;">
 			<div className="container">
 				<nav className="navbar navbar-expand-sm navbar-dark">
 					<div className="container-fluid">
-						<Link to="/home" className="navbar-brand">42 Pong</Link>
-						<div>
-							<img className="rounded-circle"
-								width="30" height="30"
-								src={profileImg} />
-							<Link to="/profile" className="navbar-brand ps-2 text-light">{name}</Link>
+						<Link to="/home" className="navbar-brand fs-4">42 Pong</Link>
+						<div className="row">
+							<div className="col">
+								<img className="rounded-circle"
+									width="35" height="35"
+									src={profileImg} />
+							</div>
+							<div className="dropdown col">
+								<div className=" btn-primary btn-sm text-center text-light fs-4" style="cursor: pointer;" data-bs-toggle="dropdown">
+									{name}
+								</div>
+								<ul className="dropdown-menu" style="cursor: pointer;">
+									<li className="dropdown-item" onClick={onClickShowMyInfo}>Show Info</li>
+									<li className="dropdown-item text-danger" onClick={onClickLogout}>Logout</li>
+								</ul>
+							</div>
 						</div>
 					</div>
 				</nav>
