71eb33647962c4ea79837ca1397cffef4a2739d7
sayoon <sayoon@student.42seoul.kr>
2024-06-21 18:40:20 +0900

    Fix: URI 수정 - me와 users 분리

--- srcs/requirements/user_server/django/user/urls.py
+++ srcs/requirements/user_server/django/user/urls.py
@@ -1,11 +1,13 @@
 
 from django.urls import path, include
-from .views import UserVeiwSet
+from .views import UserVeiwSet, friendView, me
 from rest_framework import routers
 
 userRouter = routers.DefaultRouter()
-userRouter.register(r'users', UserVeiwSet)
+userRouter.register(r'users', UserVeiwSet, basename='users')
 
 urlpatterns = [
 	path('', include(userRouter.urls)),
+	path('me', me), 
+	path('me/friend', friendView.as_view()),
 ]
