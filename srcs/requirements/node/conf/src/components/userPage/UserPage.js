85d1ff5dbe481502c6728daff9a3680928523db4
Dodontak <mu03019@naver.com>
2024-06-23 16:38:33 +0900

    refactor: StatChart.js 파일 추가
     - UserPage.js 에서 StatChart 컴포넌트 분리함.

--- srcs/requirements/node/conf/src/components/userPage/UserPage.js
+++ srcs/requirements/node/conf/src/components/userPage/UserPage.js
@@ -2,30 +2,43 @@ import { useEffect, useState, MyReact } from "../../MyReact/MyReact.js";
 import Navbar from "../Navbar.js";
 import { navigate } from "../../MyReact/MyReactRouter.js";
 import tokenRefreshAndGoTo from "../utility/tokenRefreshAndGoTo";
-import ChangeMyNicknameModal from "./ChangeMyNicknameModal.js";
-import DeleteMyAccountModal from "./DeleteMyAccountModal.js";
 import MatchRecords from "./MatchRecords.js";
 import WinRateDonut from "./WinRateDonut.js";
+import StatChart from "./StatChart.js";
 
-const defaultData = {
+const defaultData1 = {
 	"id": 0,
 	"name": "default",
 	"display_name": "display_default",
 	"email": "default@student.42seoul.kr",
 	"avatar": "https://www.studiopeople.kr/common/img/default_profile.png",
-	"exp": 1234,
-	"wins": 12,
-	"losses": 8,
-	"friends": []
+	"exp": 0,
+	"wins": 0,
+	"losses": 0,
+	"friends": [],
+	"online": false
+}
+
+const defaultData2 = {
+	"id": -1,
+	"name": "default",
+	"display_name": "display_default",
+	"email": "default@student.42seoul.kr",
+	"avatar": "https://www.studiopeople.kr/common/img/default_profile.png",
+	"exp": 0,
+	"wins": 0,
+	"losses": 0,
+	"friends": [],
+	"online": false
 }
 
 function UserPage() {
 	const queryParams = new URLSearchParams(location.search);
 	const userId = queryParams.get('userId');
-	const [myData, setMyData] = MyReact.useState(defaultData);
+	const [myData, setMyData] = MyReact.useState(defaultData1);
 	const myDataApiUrl = "http://localhost:8000/api/me";
 
-	const [userData, setUserData] = MyReact.useState(defaultData);
+	const [userData, setUserData] = MyReact.useState(defaultData2);
 	const userDataApiUrl = `http://localhost:8000/api/users/${userId}`;
 
 	MyReact.useEffect(() => {
@@ -68,85 +81,19 @@ function UserPage() {
 				console.log("in UserPage function", error);
 				navigate("/");
 			});
-	}, []);
+	}, [userId]);
 
 	return (
 		<div>
 			<Navbar position="/userpage" />
 			<div className="container text-light">
-				<StatChart myId={myData.id} userData={userData} />
+				<StatChart isMyInfo={myData.id === userData.id} myId={myData.id} userData={userData} />
 				<div className="row">
 					<div className="col-md-5 mt-3">
 						<WinRateDonut userData={userData} />
 					</div>
 					<div className="col-md-7 mt-3">
 						<MatchRecords userId={userId} />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
-}
-
-function StatChart({ myId, userData }) {
-	const level = Math.floor(userData.exp / 1000);
-	const nextexp = Math.floor((userData.exp + 1000) / 1000) * 1000;
-	const win = userData.wins;
-	const loss = userData.losses;
-	return (
-		<div>
-			<div className="container">
-				<div className="d-inline-flex">
-					<div className="p-1 fs-3">{userData.name} Info</div>
-					<div className="p-1 mt-1">
-						{myId === userData.id ? <ChangeMyNicknameModal title="Change nickname" myId={myId} /> : null}
-					</div>
-					<div className="p-1 mt-1">
-						{myId === userData.id ? <DeleteMyAccountModal title="delete Account" myId={myId} /> : null}
-					</div>
-					<div className="p-1 mt-3">
-						nickname: {userData.display_name}
-					</div>
-				</div>
-			</div>
-			<div className="container">
-				<div className="row border-top border-bottom rounded bg-secondary bg-opacity-25">
-					<div className="col-md-5">
-						<div className="container m-1 text-center">
-							<img className="rounded-circle"
-								width="120" height="120"
-								src={userData.avatar}></img>
-							<div className="container mt-1 text-center">{userData.name}</div>
-						</div>
-					</div>
-					<div className="col-md-7">
-						<div className="container m-1 text-center">
-							<div className="row mt-1 mb-1">
-								<div className="col-4">Level</div>
-								<div className="col-2">:</div>
-								<div className="col-6">{level}</div>
-							</div>
-							<div className="row mt-1 mb-1">
-								<div className="col-4">Exp</div>
-								<div className="col-2">:</div>
-								<div className="col-6">{userData.exp}/{nextexp}</div>
-							</div>
-							<div className="row mt-1 mb-1">
-								<div className="col-4">Total</div>
-								<div className="col-2">:</div>
-								<div className="col-6">{win + loss}</div>
-							</div>
-							<div className="row mt-1 mb-1">
-								<div className="col-4">Win</div>
-								<div className="col-2">:</div>
-								<div className="col-6">{win}</div>
-							</div>
-							<div className="row mt-1 mb-1">
-								<div className="col-4">Lose</div>
-								<div className="col-2">:</div>
-								<div className="col-6">{loss}</div>
-							</div>
-						</div>
 					</div>
 				</div>
 			</div>
