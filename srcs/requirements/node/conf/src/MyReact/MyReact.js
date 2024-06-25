63986422e97cb6c6f6729553ffae04b5af5b9b95
seonlim <dltjwl1@naver.com>
2024-06-25 16:04:28 +0900

    fixed commitDeletion bug

--- srcs/requirements/node/conf/src/MyReact/MyReact.js
+++ srcs/requirements/node/conf/src/MyReact/MyReact.js
@@ -79,7 +79,17 @@ function updateDom(dom, prevProps, nextProps) {
 }
 
 function commitRoot() {
-  deletions.forEach(commitWork);
+  deletions.forEach((fiber) => {
+    if (!fiber) {
+      return;
+    }
+    let domParentFiber = fiber.parent;
+    while (!domParentFiber.dom) {
+      domParentFiber = domParentFiber.parent;
+    }
+    const domParent = domParentFiber.dom;
+    commitDeletion(fiber, domParent);
+  });
   commitWork(wipRoot.child);
   currentRoot = wipRoot;
   wipRoot = null;
@@ -97,11 +107,12 @@ function commitWork(fiber) {
   if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
     domParent.appendChild(fiber.dom);
   }
-  else if (fiber.effectTag === "DELETION") {
-    commitDeletion(fiber, domParent);
-    commitWork(fiber.sibling);
-    return;
-  }
+  // else if (fiber.effectTag === "DELETION") {
+  //   commitDeletion(fiber, domParent);
+  //   fiber.effectTag = "DELETED";
+  //   commitWork(fiber.sibling);
+  //   return;
+  // }
   else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
     updateDom(fiber.dom, fiber.alternate.props, fiber.props);
   }
@@ -222,7 +233,6 @@ export function useState(initial) {
       wipRoot = tmpRoot;
       nextUnitOfWork = wipRoot;
       deletions = [];
-
     }
     else {
       setStateQueue.push({
