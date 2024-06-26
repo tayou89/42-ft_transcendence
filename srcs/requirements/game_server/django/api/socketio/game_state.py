a177f6f7280fc7412702344ac4d629fcdcb00c2a
sayoon <sayoon@student.42seoul.kr>
2024-06-26 23:01:06 +0900

    Chore: 게임 정보 수정 - 패들 크기 조정 60 -> 120 - 공 속도 조정: 최대 20 -> 최대 30 - 공 속도가 빠를 때, 한 패들에 공이 두번 튕기는 현상 수정

--- srcs/requirements/game_server/django/api/socketio/game_state.py
+++ srcs/requirements/game_server/django/api/socketio/game_state.py
@@ -20,34 +20,48 @@ P2 = 1
 
 def calculate_bounce_angle(ball_dir, ball_y, paddle_y) -> list:
 
-	relative_intersect_y = (ball_y - paddle_y) / (BAR_HEIGHT / 2)
+	relative_intersect_y = ((ball_y + BALL_RADIUS) - (paddle_y + BAR_HEIGHT // 2)) / (BAR_HEIGHT / 2)
 	
-	max_bounce_angle = math.pi / 2  # 최대 반사각도 (예: 45도)
+	max_bounce_angle = math.pi / 3
 	bounce_angle = relative_intersect_y * max_bounce_angle
 
 	new_direction_x = math.cos(bounce_angle)
 	new_direction_y = math.sin(bounce_angle)
 
-	return [new_direction_x, new_direction_y]
+	if ball_dir[X] < 0:
+		return [new_direction_x, new_direction_y]
+	else:
+		return [-new_direction_x, new_direction_y]
 
-	# if ball_dir[X] < 0:
-		# return [math.cos(bounce_angle), math.sin(bounce_angle)]
-	# else:
-	# 	return [-math.cos(bounce_angle), math.sin(bounce_angle)]
 
-def get_distance(ball, bar) -> list:
-	ball_x, ball_y = ball	
-	bar_x, bar_y = bar
+def check_collision(ball, bar):
+    
+	ball_left = ball[X]
+	ball_right = ball[X] + BALL_RADIUS * 2
+	ball_top = ball[Y]
+	ball_bottom = ball[Y] + BALL_RADIUS * 2
 
-	distance = math.sqrt((ball_x - bar_x) ** 2 + (ball_y - bar_y) ** 2)
-	return distance
+	bar_left = bar[X]
+	bar_right = bar[X] + BAR_WIDTH
+	bar_top = bar[Y]
+	bar_bottom = bar[Y] + BAR_HEIGHT
+
+	if ball_bottom < bar_top or ball_top > bar_bottom:
+		return False
+
+	if ball_right < bar_left or ball_left > bar_right:
+		return False
+
+	return True
+
 
 def clamp(value, min_value, max_value):
 	return max(min(value, max_value), min_value)
 
+
 class GameState:
 	def __init__(self):
-		self.ball_dir = [1, 0]
+		self.ball_dir = [-1, 0]
 		self.ball_speed = 5
 
 		self.p1_pid = 0
@@ -71,13 +85,11 @@ class GameState:
 			self.p2_dir = direction
 
 	def next_frame(self) -> None:
-		if self.pause == 0:
-			self.move_ball()
-		else:
+		if self.pause > 0:
 			self.pause -= 1
-		self.move_player()
-		if self.ball_speed < 20:
-			self.ball_speed += 0.1
+		else:
+			self.move_ball()
+			self.move_player()
 		return {
 			"ball": self.ball_coords,
 			"paddle": [self.p1_coords[1], self.p2_coords[1]],
@@ -101,12 +113,16 @@ class GameState:
 
 		self.check_wall_collision()
   
+		if self.ball_speed < 30:
+			self.ball_speed += 0.2
+  
 
 	def check_paddle_collision(self):
-		if get_distance(self.ball_coords, self.p1_coords) < BALL_RADIUS:
+
+		if self.ball_dir[X] < 0 and check_collision(self.ball_coords, self.p1_coords):
 			self.ball_dir = calculate_bounce_angle(self.ball_dir, self.ball_coords[Y], self.p1_coords[Y])
 
-		if get_distance(self.ball_coords, self.p2_coords) < BALL_RADIUS:
+		if self.ball_dir[X] > 0 and check_collision(self.ball_coords, self.p2_coords):
 			self.ball_dir = calculate_bounce_angle(self.ball_dir, self.ball_coords[Y], self.p2_coords[Y])
    
 	def check_wall_collision(self):
@@ -121,10 +137,10 @@ class GameState:
 		self.init_position()
 		self.pause = 20
 		if player == P1:
-			self.ball_dir = [-1, 0]
+			self.ball_dir = [1, 0]
 			self.score[P1] += 1
 		elif player == P2:
-			self.ball_dir = [1, 0]
+			self.ball_dir = [-1, 0]
 			self.score[P2] += 1
 		self.ball_speed = 5
 
