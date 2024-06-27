37a07a72fcf3e4d278e792e9b1ce6fa58f9ad7fa
sayoon <sayoon@student.42seoul.kr>
2024-06-27 16:34:19 +0900

    Feat: gameserver 보안 인증 추가 - gameserver jwt 인증 추가 - 인증 과정에서 사용한 url 변경

--- srcs/requirements/game_server/django/api/views.py
+++ srcs/requirements/game_server/django/api/views.py
@@ -1,36 +1,15 @@
 
-from rest_framework.response import Response
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from rest_framework.renderers import JSONRenderer
 from .authentication import CustomPermission
 from .models import Room, RoomSerializer
-from django.shortcuts import get_object_or_404
-
-from django.shortcuts import render
 
 class RoomViewset(viewsets.ModelViewSet):
 	renderer_classes = [JSONRenderer]
-	# permission_classes = [CustomPermission]
+	permission_classes = [CustomPermission]
+	http_method_names = ['get', 'post']
 	queryset = Room.objects.all()
 	serializer_class = RoomSerializer
-	
-
-# class RoomViewset(viewsets.ViewSet):
-# 	renderer_classes = [JSONRenderer]
-# 	# permission_classes = [CustomPermission]
- 
-# 	def list(self, request):
-# 		queryset = Room.objects.all()
-# 		serializer = RoomSerializer(queryset, many=True)
-# 		return Response(serializer.data)
-
-# 	def create(self, request):
-# 		serializer = RoomSerializer(data=request.data)
-# 		if serializer.is_valid():
-# 			serializer.save()
-# 			return Response(serializer.data, status=status.HTTP_201_CREATED)
-# 		else:
-# 			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # Create your views here.
