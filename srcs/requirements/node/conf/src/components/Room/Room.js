3d88846cdca4d05520638c8e3298e25a90acd88d
tayou89 <ysh00718@gmail.com>
2024-07-04 16:13:33 +0900

    Fix: 로컬 게임 투명공 에러 수정

--- srcs/requirements/node/conf/src/components/Room/Room.js
+++ srcs/requirements/node/conf/src/components/Room/Room.js
@@ -1,9 +1,11 @@
 import { useEffect, useState, MyReact } from "../../MyReact/MyReact.js";
+import { navigate } from "../../MyReact/MyReactRouter.js";
 import TopLine from "./TopLine.js";
 import Title from "./Title.js";
 import Player from "./Player.js";
 import BottomLine from "./BottomLine.js";
 import QuitPopUp from "./QuitPopUp.js";
+import getRoomsData from "../utility/getRoomsData.js";
 import { pongSocket, mttSocket } from "../RemoteGame/GameSocket.js";
 import { GAME } from "../RemoteGame/constant.js";
 import "../../css/room/room.css";
@@ -12,8 +14,17 @@ function Room() {
     const [ room, setRoom ] = useState(getInitialRoomData());
 
     useEffect(() => {
-        room.socket.sendRoomJoinMessage(room.myId, room.title);
-    	room.socket.turnOnRoomChannel(setRoom);
+        const executeRoomFunctions = async () => {
+            const exists = await isExistedRoom(room.roomId);
+
+            if (!exists) 
+                navigate("/home");
+            else {
+                room.socket.sendRoomJoinMessage(room.myId, room.title);
+                room.socket.turnOnRoomChannel(setRoom);
+            }
+        };
+        executeRoomFunctions();
     }, []);
     return (
         <div className="container-fluid" id="room-page">
@@ -31,6 +42,7 @@ function getInitialRoomData() {
     const URLData = new URLSearchParams(queryString);
 
     return ({ 
+        roomId: Number(URLData.get('roomId')),
         title: URLData.get('title'),
         myId: Number(URLData.get('myId')),
         type: URLData.get('type'),
@@ -38,6 +50,12 @@ function getInitialRoomData() {
         isQuitClicked: false,
         players: URLData.get('type') === GAME.TYPE.PONG ? [{}, {}] : [{}, {}, {}, {}],
     });
+}
+
+async function isExistedRoom(roomId) {
+    const roomData = await getRoomsData();
+
+    return (roomData.some((room) => room.id === roomId));
 }
 
 export default Room
