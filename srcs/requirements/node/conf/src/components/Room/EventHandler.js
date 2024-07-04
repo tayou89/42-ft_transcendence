51c229cc6aabbe12f8bd35173003393573178c57
tayou89 <ysh00718@gmail.com>
2024-07-04 17:13:26 +0900

    Fix: 동일한 계정으로 같은 방입장 방지되도록 수정

--- srcs/requirements/node/conf/src/components/Room/EventHandler.js
+++ srcs/requirements/node/conf/src/components/Room/EventHandler.js
@@ -1,15 +1,17 @@
 import { MyReact } from "../../MyReact/MyReact.js";
 import RoomStateSetter from "./StateSetter.js";
 import logout from "../utility/logout.js";
+import { SOCKET } from "../RemoteGame/constant.js";
 
 class RoomSocketEventHandler {
-    setRoomEvent(setPlayers) {
+    setRoomEvent(setPlayers, socket) {
         this.#roomEvent = async (newPlayers) => {
             try {
                 await RoomStateSetter.setPlayers(newPlayers, setPlayers);
             }
             catch (error) {
                 console.log("Room Error:", error);
+                socket.emit(SOCKET.EVENT.LEAVE_ROOM);
                 logout();
             }
         };
