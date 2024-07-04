959ecff19efbd5dc0d1c314c1a3700d36bcf1381
tayou89 <ysh00718@gmail.com>
2024-07-04 17:56:26 +0900

    Fix: 프롭스로 데이터 받는 형식으로 변경

--- srcs/requirements/node/conf/src/components/Room/Room.js
+++ srcs/requirements/node/conf/src/components/Room/Room.js
@@ -5,72 +5,47 @@ import Title from "./Title.js";
 import Player from "./Player.js";
 import BottomLine from "./BottomLine.js";
 import QuitPopUp from "./QuitPopUp.js";
-import getRoomsData from "../utility/getRoomsData.js";
 import { pongSocket, mttSocket } from "../RemoteGame/GameSocket.js";
 import { GAME } from "../RemoteGame/constant.js";
 import "../../css/room/room.css";
 
-function Room() {
-    const [ room, setRoom ] = useState(getInitialRoomData());
+function Room(props) {
+    try {
+        if (!props)
+            throw new Error("Can't find room data");
+        const [ room, setRoom ] = useState(getInitialRoomData(props));
 
-    useEffect(() => {
-        const executeRoomFunctions = async () => {
-            try {
-                const roomsData = await getRoomsData();
-                const myRoomData = getMyRoomData(roomsData, room.roomId);
-
-                checkSamePlayer(myRoomData, room.myId);
-                room.socket.sendRoomJoinMessage(room.myId, room.title);
-                room.socket.turnOnRoomChannel(setRoom);
-            }
-            catch (error) {
-                alert(error);
-                navigate("/home");
-            }
-        };
-        executeRoomFunctions();
-    }, []);
-    return (
-        <div className="container-fluid" id="room-page">
-            <TopLine />
-            <Title room={ room } />
-            <Player room={ room } />
-            <BottomLine setFunction={ setRoom } />
-            <QuitPopUp data={ room } setFunction={ setRoom } /> 
-        </div>
-    );
+        useEffect(() => {
+            room.socket.sendRoomJoinMessage(room.myId, room.title);
+            room.socket.turnOnRoomChannel(setRoom);
+        }, []);
+        return (
+            <div className="container-fluid" id="room-page">
+                <TopLine />
+                <Title room={ room } />
+                <Player room={ room } />
+                <BottomLine setFunction={ setRoom } />
+                <QuitPopUp data={ room } setFunction={ setRoom } /> 
+            </div>
+        );
+    }
+    catch (error) {
+        alert(error);
+        navigate("/home");
+    }
 }
 
-function getInitialRoomData() {
-    const queryString = window.location.search;
-    const URLData = new URLSearchParams(queryString);
+function getInitialRoomData(props) {
+    const room = props.room;
 
     return ({ 
-        roomId: Number(URLData.get('roomId')),
-        title: URLData.get('title'),
-        myId: Number(URLData.get('myId')),
-        type: URLData.get('type'),
-        socket: URLData.get('type') === GAME.TYPE.PONG ? pongSocket : mttSocket,
+        title: room.title,
+        type: room.type,
+        myId: props.myId,
+        socket: room.type === GAME.TYPE.PONG ? pongSocket : mttSocket,
         isQuitClicked: false,
-        players: URLData.get('type') === GAME.TYPE.PONG ? [{}, {}] : [{}, {}, {}, {}],
+        players: room.type === GAME.TYPE.PONG ? [{}, {}] : [{}, {}, {}, {}],
     });
-}
-
-function getMyRoomData(roomsData, roomId) {
-    for (let room of roomsData) {
-        if (room.id === roomId)
-            return (room);
-    }
-    throw new Error("Room does not exist");
-}
-
-function checkSamePlayer(roomData, myId) {
-    const roomKeysToCheck = [ "p1", "p2", "p3", "p4" ];
-
-    for (let key of roomKeysToCheck) {
-        if (roomData[key] === myId)
-            throw new Error("Can't enter: you already joined the same room");
-    }
 }
 
 export default Room;
