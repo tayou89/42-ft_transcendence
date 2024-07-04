897e4fa83ab0642e2ba09d9aa9d8c7382477002f
Dodontak <mu03019@naver.com>
2024-07-04 13:31:27 +0900

    refactor: OTP notify

--- srcs/requirements/node/conf/src/components/EmailOTP.js
+++ srcs/requirements/node/conf/src/components/EmailOTP.js
@@ -1,10 +1,30 @@
 import { useState, MyReact } from "../MyReact/MyReact.js";
 import { navigate } from "../MyReact/MyReactRouter.js";
+import notifyStatusById from "./utility/notifyStatusById.js";
 
-async function onClickSubmit(event) {
+function EmailOTP() {
+	return (
+		<div className="text-light text-center" style="user-select: none;">
+			<div className="py-5"></div>
+			<div className="container fs-1 py-5">
+				42 Pong
+			</div>
+			<div className="container">
+			</div>
+			<div className="container">42Seoul에 연동된 이메일로 인증코드를 보냈습니다.</div>
+			<div className="container">6자리 인증코드를 입력하세요.</div>
+			<form className="container my-1 py-1">
+				<input id="otp-code-input" type="text" placeholder="message you received" maxLength={6} autocomplete="off" />
+				<button className="btn btn-primary btn-sm" onClick={onClickSubmitOtpCode}>Submit</button>
+				<div id="otp-status-message" className="text-danger"></div>
+			</form>
+		</div>
+	);
+}
+
+async function onClickSubmitOtpCode(event) {
 	event.preventDefault();
-	const input = event.target.parentNode.querySelector("input");
-	const authStatusMessage = document.querySelector("#auth-status-message");
+	const input = document.querySelector("#otp-code-input");
 	try {
 		const response = await fetch("http://localhost:8000/api/otp", {
 			method: 'POST',
@@ -20,32 +40,12 @@ async function onClickSubmit(event) {
 		if (data.result === "success") {
 			navigate("/");
 		} else {
-			authStatusMessage.innerText = data.result;
+			notifyStatusById(data.result, false, "otp-status-message");
 		}
 	} catch (error) {
-		authStatusMessage.innerText = "Network Error!";
-		setTimeout(() => { navigate("/") }, 800);
+		notifyStatusById("Network Error!", false, "otp-status-message");
+		setTimeout(() => { navigate("/login") }, 800);
 	}
-}
-
-function EmailOTP() {
-	return (
-		<div className="text-light text-center" style="user-select: none;">
-			<div className="py-5"></div>
-			<div className="container fs-1 py-5">
-				42 Pong
-			</div>
-			<div className="container">
-			</div>
-			<div className="container">42Seoul에 연동된 이메일로 인증코드를 보냈습니다.</div>
-			<div className="container">6자리 인증코드를 입력하세요.</div>
-			<form className="container my-1 py-1">
-				<input id="auth-code-input" type="text" placeholder="message you received" maxLength={6} autocomplete="off" />
-				<button className="btn btn-primary btn-sm" onClick={onClickSubmit}>Submit</button>
-				<div id="auth-status-message" className="text-danger"></div>
-			</form>
-		</div>
-	);
 }
 
 export default EmailOTP
